46189de5c2035a9159955a2c795652a52d9032e5
Viktor Turkovics <arturo.el.guerrero.23@gmail.com>
2020-06-03 17:57:07 +0200

    Animal extended

--- week-03/day-02/src/animal/Main.java
+++ week-03/day-02/src/animal/Main.java
@@ -2,6 +2,15 @@ package animal;
 
 public class Main {
     public static void main(String[] args) {
-
+        Animal a1 = new Animal();
+        System.out.println("a1 thirst before playing: " + a1.thirst);
+        System.out.println("a1 hunger before playing: " + a1.hunger);
+        a1.play();
+        System.out.println("a1 thirst after playing: " + a1.thirst);
+        System.out.println("a1 hunger after playing: " + a1.hunger);
+        a1.drink();
+        a1.eat();
+        System.out.println("a1 thirst after drinking: " + a1.thirst);
+        System.out.println("a1 hunger after eating: " + a1.hunger);
     }
 }
